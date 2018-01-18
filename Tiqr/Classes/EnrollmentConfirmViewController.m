4f87a97e1aedf7532e333d779e306885ac596df6
Erik van der Wal <erikvdwal@gmail.com>
2018-01-18 15:34:54 +0100

    Rename method to be more consistent

--- Tiqr/Classes/EnrollmentConfirmViewController.m
+++ Tiqr/Classes/EnrollmentConfirmViewController.m
@@ -88,7 +88,7 @@
 
 - (IBAction)ok {
     if (ServiceContainer.sharedInstance.secretService.biometricIDAvailable) {
-        [self useTouchID];
+        [self useBiometricID];
     } else {
         [self usePIN];
     }
@@ -100,7 +100,7 @@
     [self.navigationController pushViewController:viewController animated:YES];
 }
 
-- (void)useTouchID {
+- (void)useBiometricID {
     [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
     
     [ServiceContainer.sharedInstance.challengeService
